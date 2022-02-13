ae487ce2b90bd3dbaa2be14428e45376a93434a9
Aren4o <95856526+Aren4o@users.noreply.github.com>
2022-02-13 23:19:24 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let Name = prompt("What is your name?");
+let Education = prompt("What is your educational background?");
+let Age = prompt("How old are you?");
+let Language = prompt("How many languages do you know?");
+let Hobbies = prompt("What about your hobbies?");
+
+alert("Nice to meet you" + " " + Name);
+
+let arr = [Name,Education,Age,Language,Hobbies];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
